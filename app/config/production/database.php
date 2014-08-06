68fa012f612babd24751be3334d3e00aacf1e094
Steph Dube <scdube7@gmail.com>
2014-08-06 02:30:23 -0400

    config prob

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -8,7 +8,7 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => '127.9.136.2',
+            'host'      => '127.9.137.2',
             'database'  => 'meta',
             'username'  => 'adminajFRvhw',
             'password'  => 'uyBBk4A3V3pN',
