a0091e668d94df8d026b7adf789ff15c9d477e14
Steph Dube <scdube7@gmail.com>
2014-08-06 00:48:48 -0400

    for openshift

--- app/routes.php
+++ app/routes.php
@@ -114,27 +114,3 @@ Route::get('/debug', function() {
     echo '</pre>';
 
 });
-
-Route::get('/seed', function(){
-			// Import data for Albums table
-		DB::raw('LOAD DATA INFILE "C:/MAMP/htdocs/p4/metalData/MAAlbums.csv" INTO TABLE albums FIELDS TERMINATED BY ";;" LINES TERMINATED BY "\r\n"');
-		DB::table('albums')->where('album_id', '=', 0)
-			->update(array('album_id'=>1));
-
-		// Import data for Bands table
-		DB::raw('LOAD DATA INFILE "C:/MAMP/htdocs/p4/metalData/MABands.csv" INTO TABLE bands FIELDS TERMINATED BY ";;;" LINES TERMINATED BY "\r\n"');
-		DB::table('bands')->where('band_id', '=', 0)
-			->update(array('band_id'=>1));
-
-		// Import data for Countries table
-		DB::raw('LOAD DATA INFILE "C:/MAMP/htdocs/p4/metalData/countries.csv" INTO TABLE countries FIELDS TERMINATED BY "," LINES TERMINATED BY "\r\n"');
-
-		// Import data for Labels table
-		DB::raw('LOAD DATA INFILE "C:/MAMP/htdocs/p4/metalData/labels.csv" INTO TABLE labels FIELDS TERMINATED BY "," LINES TERMINATED BY "\r\n"');
-
-		// Import data for Reviews table
-		DB::raw('LOAD DATA INFILE "C:/MAMP/htdocs/p4/metalData/MAReviews.csv" INTO TABLE reviews FIELDS TERMINATED BY "," LINES TERMINATED BY "\r\n"');
-
-		// Import data for Similar Bands table
-		DB::raw('LOAD DATA INFILE "C:/MAMP/htdocs/p4/metalData/MASimilarArtists.csv" INTO TABLE similar_bands FIELDS TERMINATED BY "," LINES TERMINATED BY "\r\n"');
-});
